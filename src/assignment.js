18fa132aba8a9eb4a5953baa2b79676deaadeadb
blessing lartey <74651333+BlessingLartey@users.noreply.github.com>
2022-07-13 18:36:06 +0000

    challenges

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  console.log(arrayOfNumbers)
+  let sum = 0;
+  for( i=0; i < arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers [i]
+  }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +32,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(numbers) {
+let evenNumbers=numbers.filter((n) => n % 2 ==0);
+    return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
